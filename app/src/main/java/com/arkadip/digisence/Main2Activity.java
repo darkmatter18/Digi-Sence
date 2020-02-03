1772923f0a17582b02bca19f33b3feaf128d8e04
Arkadip <in2arkadipb13@gmail.com>
2020-02-03 12:58:11 +0530

    feat: Added permission for camera - Updated Junit
    Signed-off-by: Arkadip <in2arkadipb13@gmail.com>

--- app/src/main/java/com/arkadip/digisence/Main2Activity.java
+++ app/src/main/java/com/arkadip/digisence/Main2Activity.java
@@ -1,17 +1,40 @@
 package com.arkadip.digisence;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
 import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 
 public class Main2Activity extends AppCompatActivity {
+
     private static String[] PERMISSIONS = {
             Manifest.permission.CAMERA
     };
+
+    private static boolean hasPermissions(Context context, String... permissions) {
+        if (context != null && permissions != null) {
+            for (String permission : permissions) {
+                if (ActivityCompat.checkSelfPermission(context, permission) != PackageManager.PERMISSION_GRANTED) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+        if (!hasPermissions(this, PERMISSIONS)) {
+            int PERMISSION_ALL = 1;
+            ActivityCompat.requestPermissions(this, PERMISSIONS, PERMISSION_ALL);
+        }
+
     }
+
 }
