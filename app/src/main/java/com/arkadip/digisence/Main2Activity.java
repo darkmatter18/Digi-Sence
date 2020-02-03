aeab24fc0d7306a2905ce5df7e5d52a59604162a
Arkadip <in2arkadipb13@gmail.com>
2020-02-03 15:30:48 +0530

    feat: Camera Viewing Working Properly
    Signed-off-by: Arkadip <in2arkadipb13@gmail.com>

--- app/src/main/java/com/arkadip/digisence/Main2Activity.java
+++ app/src/main/java/com/arkadip/digisence/Main2Activity.java
@@ -1,14 +1,71 @@
 package com.arkadip.digisence;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
 
 import android.Manifest;
 import android.content.Context;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.camera.core.CameraSelector;
+import androidx.camera.core.Preview;
+import androidx.camera.lifecycle.ProcessCameraProvider;
+import androidx.camera.view.PreviewView;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.lifecycle.LifecycleOwner;
+
+import com.google.common.util.concurrent.ListenableFuture;
+
+import java.util.concurrent.ExecutionException;
+
 public class Main2Activity extends AppCompatActivity {
+    private ListenableFuture<ProcessCameraProvider> cameraProviderListenableFuture;
+    PreviewView previewView;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+        previewView = findViewById(R.id.preview_view);
+
+        if (!hasPermissions(this, PERMISSIONS)) {
+            int PERMISSION_ALL = 1;
+            ActivityCompat.requestPermissions(this, PERMISSIONS, PERMISSION_ALL);
+        } else {
+            startCamera();
+        }
+    }
+
+    private void startCamera() {
+        cameraProviderListenableFuture = ProcessCameraProvider.getInstance(this);
+        cameraProviderListenableFuture.addListener(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    ProcessCameraProvider cameraProvider = cameraProviderListenableFuture.get();
+                    bindPreview(cameraProvider);
+                } catch (ExecutionException | InterruptedException e) {
+                    //No error Should be there
+                }
+            }
+        }, ContextCompat.getMainExecutor(this));
+    }
+
+    private void bindPreview(@NonNull ProcessCameraProvider cameraProvider) {
+        Preview preview = new Preview.Builder()
+                .setTargetName("Preview")
+                .build();
+        preview.setPreviewSurfaceProvider(previewView.getPreviewSurfaceProvider());
+
+        CameraSelector cameraSelector = new CameraSelector.Builder()
+                .requireLensFacing(CameraSelector.LENS_FACING_BACK)
+                .build();
+        cameraProvider.bindToLifecycle((LifecycleOwner) this, cameraSelector, preview);
+    }
 
     private static String[] PERMISSIONS = {
             Manifest.permission.CAMERA
@@ -24,17 +81,4 @@ public class Main2Activity extends AppCompatActivity {
         }
         return true;
     }
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main2);
-
-        if (!hasPermissions(this, PERMISSIONS)) {
-            int PERMISSION_ALL = 1;
-            ActivityCompat.requestPermissions(this, PERMISSIONS, PERMISSION_ALL);
-        }
-
-    }
-
 }
