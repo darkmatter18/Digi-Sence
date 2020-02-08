80dc91a830b494534b25e897616658c9008380b7
Arkadip <in2arkadipb13@gmail.com>
2020-02-08 16:13:50 +0530

    feat: Changed the Predict Method - Preprocessor function now uses bitmap - Changed some access modifiers
    Signed-off-by: Arkadip <in2arkadipb13@gmail.com>

--- app/src/main/java/com/arkadip/digisence/Classifier.java
+++ app/src/main/java/com/arkadip/digisence/Classifier.java
@@ -1,49 +1,42 @@
 package com.arkadip.digisence;
 
-import android.media.Image;
+import android.graphics.Bitmap;
 
 import org.pytorch.IValue;
 import org.pytorch.Module;
 import org.pytorch.Tensor;
 import org.pytorch.torchvision.TensorImageUtils;
 
-public class Classifier {
-    Module model;
+class Classifier {
+    private Module model;
 
-    private float[] mean = {0.5f, };
-    private float[] std = {0.5f, };
+    private float[] mean = {0.5f, 0.5f, 0.5f};
+    private float[] std = {0.5f, 0.5f, 0.5f};
 
-    public Classifier(String modelPath){
+    Classifier(String modelPath) {
         model = Module.load(modelPath);
     }
 
-    public void setMeanAndStd(float[] mean, float[] std) {
-        this.mean = mean;
-        this.std = std;
+    private Tensor preprocessor(Bitmap bitmap) {
+        return TensorImageUtils.bitmapToFloat32Tensor(bitmap, this.mean, this.std);
     }
 
-    private Tensor preprocess(Image image){
-        return TensorImageUtils.imageYUV420CenterCropToFloat32Tensor(image, 0,
-                28, 28,this.mean, this.std);
-    }
-
-    private int argMax(float[] inputs){
+    private int argMax(float[] inputs) {
         int maxIndex = -1;
         float maxvalue = 0.0f;
-        for (int i = 0; i < inputs.length; i++){
-            if(inputs[i] > maxvalue) {
+        for (int i = 0; i < inputs.length; i++) {
+            if (inputs[i] > maxvalue) {
                 maxIndex = i;
                 maxvalue = inputs[i];
             }
         }
         return maxIndex;
     }
-    public int predict(Image image){
-        Tensor imageTensor = preprocess(image);
-        Tensor output = model.forward(IValue.from(imageTensor)).toTensor();
-        float[] scores = output.getDataAsFloatArray();
 
+    int predict(Bitmap bitmap) {
+        Tensor tensor = preprocessor(bitmap);
+        Tensor output = model.forward(IValue.from(tensor)).toTensor();
+        float[] scores = output.getDataAsFloatArray();
         return argMax(scores);
     }
-
 }
